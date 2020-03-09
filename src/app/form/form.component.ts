bb345bb0054a234a0f255f08d907bc7194239257
OriannaCapote <oriannacapoteopsu@gmail.com>
2020-03-08 23:38:10 -0400

    Perfeccionando detalles

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  optionsSelect: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.optionsSelect = [
+      { value: '1', label: 'Option 1' },
+      { value: '2', label: 'Option 2' },
+      { value: '3', label: 'Option 3' },
+      ];
   }
 
 }
